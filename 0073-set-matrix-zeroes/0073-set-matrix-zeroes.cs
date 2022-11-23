cc03d1b5e1a0a5a49c3a534580271ed88d1c1386
Stephen Leong <775208472@qq.com>
2022-11-24 00:15:43 +0800

    Time: 319 ms (6.73%), Space: 46.2 MB (56.73%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public void SetZeroes(int[][] matrix) {
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+        bool[] r = new bool[m];
+        bool[] c = new bool[n];
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                var val = matrix[i][j];
+                if (val == 0)
+                {
+                    r[i] = true;
+                    c[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if(r[i] || c[j])
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+}
