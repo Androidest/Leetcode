b88f50eb8883940c5ee9d1aaf1807d393fc6bd33
Stephen Leong <775208472@qq.com>
2022-11-26 20:03:06 +0800

    Time: 47 ms (26.67%), Space: 25.2 MB (82.00%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    int[,] Memo;
+    int M;
+    int N;
+    public int UniquePaths(int m, int n) {
+        Memo = new int[m, n];
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                Memo[i, j] = -1;
+        M = m - 1;
+        N = n - 1;
+        return GetPathsRecursively(0, 0);
+    }
+    
+    int GetPathsRecursively(int i, int j)
+    {
+        if (i > M || j > N) 
+            return 0;
+
+        if (i == M && j == N)
+            return 1;
+
+        if (Memo[i, j] != -1)
+            return Memo[i, j];
+        Memo[i, j] = GetPathsRecursively(i+1, j) + 
+                     GetPathsRecursively(i, j+1);
+        return Memo[i, j];
+    }
+}
