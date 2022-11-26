fcb45a1e7fd8c236bed99486772d6bc15686d5ef
Stephen Leong <775208472@qq.com>
2022-11-26 18:55:33 +0800

    Time: 230 ms (42.66%), Space: 43.4 MB (95.95%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -0,0 +1,50 @@
+public class Solution {    
+    int M;
+    int N;
+    char[][] Grid;
+    Queue<(int, int)> PosQueue;
+    
+    public int NumIslands(char[][] grid) {
+        M = grid.Length;
+        N = grid[0].Length;
+        int count = 0;
+        Grid = grid;
+        
+        for (int i = 0; i < M; ++i)
+        {
+            for (int j = 0; j < N; ++j)
+            {
+                if (Grid[i][j] == '1')
+                {
+                    ++count;
+                    InfectIsland_BFS(i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    void InfectIsland_BFS(int i, int j)
+    {
+        PosQueue = new Queue<(int, int)>();
+        InfectValidIsland(i, j);
+        
+        while(PosQueue.Count > 0)
+        {
+            (i, j) = PosQueue.Dequeue();            
+            InfectValidIsland(i+1, j);
+            InfectValidIsland(i-1, j);
+            InfectValidIsland(i, j+1);
+            InfectValidIsland(i, j-1);
+        }
+    }
+    
+    void InfectValidIsland(int i, int j)
+    {
+        if (0 <= i && i < M && 0 <= j && j < N && Grid[i][j] == '1') 
+        {
+            Grid[i][j] = '0';
+            PosQueue.Enqueue((i, j));
+        }
+    }
+}
