c77dfcc596ba264af97a5643250844f32a9e6bed
Stephen Leong <775208472@qq.com>
2022-11-28 10:11:56 +0800

    Time: 421 ms (40.31%), Space: 48.9 MB (69.27%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = nums[0];
+        foreach (int num in nums)
+        {
+            sum = Math.Max(num + sum, num);
+            maxSum = Math.Max(maxSum, sum);
+        }
+        return maxSum;
+    }
+}
